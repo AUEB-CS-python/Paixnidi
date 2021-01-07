9c195663ed129ab4ae6e4cb8b3281b38128ac215
NightDagger7 <76915728+NightDagger7@users.noreply.github.com>
2021-01-07 16:37:10 +0200

    Add files via upload

---
+++ vathmoi.py
@@ -0,0 +1,21 @@
+sum=0
+if cards==['A'] :
+    p=1
+    sum+=p
+elif cards==[str(i) for i in range(2, 11)]:
+    p==i
+    sum+=p
+elif cards==['J', 'Q', 'K'] :
+    if cards==['J'] :
+        p=10
+        sum+=p
+        #play again
+    elif cards==['K']:
+        p=10
+        sum+=p
+        #xanei seira o epomenos
+    elif cards == ['K'] + ['Q'] :
+        #anoigeis kai 3h karta
+        #an tautizontai pairnei p==10
+        p==10
+return sum
